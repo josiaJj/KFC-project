f69180818e97f06cdab6bfdd7704369d22dfff5b
josiaJj <hei.josia@gmail.com>
2024-06-02 19:21:17 +0300

    init: initialize environment

---
+++ src/main/java/school/hei/kfc/kfcproject/KfcProjectApplication.java
@@ -0,0 +1,13 @@
+package school.hei.kfc.kfcproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KfcProjectApplication.class, args);
+	}
+
+}
